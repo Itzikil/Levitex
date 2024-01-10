50ec90eea78c69519751d420aa30db0d31aff3b3
itzhake <itzikil@icloud.com>
2024-01-10 20:42:57 +0200

    1.3

--- src/components/app-header.vue
+++ src/components/app-header.vue
@@ -1,23 +1,53 @@
 <template>
     <header class="flex space align-center">
-        <RouterLink to="/">
+        <RouterLink to="/" class="hide-on-mobile">
             <h2>Levitex</h2>
         </RouterLink>
         <div class="show-on-mobile mobile-nav">
-            <RouterLink to="/cart">Cart ({{ cartItems }})</RouterLink>
-            <img src="../assets/imgs/mobile-nav.png" alt=""  @click="openNav = !openNav">
+            <img src="../assets/imgs/mobile-nav.svg" alt="" @click="openNav = !openNav">
+            <RouterLink to="/">
+                <h2>Levitex</h2>
+            </RouterLink>
+            <RouterLink to="/cart" class="cart-container">
+                <img src="../assets/imgs/shopping-cart.svg" alt="">
+                <p>{{ cartItems }}</p>
+            </RouterLink>
         </div>
         <nav :class="openNav ? 'open-nav' : ''" @click="handleNavClick">
             <div class="show-on-mobile nav-logo">
-                <p @click="openNav = !openNav">X</p>
+                <RouterLink to="/">
+                    <h2>Levitex</h2>
+                </RouterLink>
+                <img src="../assets/imgs/x.svg" alt="" @click="openNav = !openNav">
             </div>
-            <RouterLink to="/textile">Bedding</RouterLink>
-            <RouterLink to="/cart">Cart ({{ cartItems }})</RouterLink>
-            <RouterLink v-if="!loggedinUser" to="/login" @click="logout()">Log in</RouterLink>
-            <RouterLink v-if="!loggedinUser" to="/signup">Signup</RouterLink>
-            <RouterLink :to="`/user/${loggedinUser._id}`" v-else class="anchor">{{ loggedinUser.username }}</RouterLink>
-            <RouterLink to="/about">About</RouterLink>
-            <RouterLink v-if="loggedinUser" to="/" @click="logout()">Log out</RouterLink>
+            <RouterLink to="/textile" class="icon-container">
+                <img src="../assets/imgs/bed.svg" alt="">
+                <p>Bedding</p>
+            </RouterLink>
+            <RouterLink to="/cart" class="icon-container">
+                <img src="../assets/imgs/shopping-cart.svg" alt="">
+                <p>Cart ({{ cartItems }})</p>
+            </RouterLink>
+            <RouterLink v-if="!loggedinUser" to="/login" @click="logout()" class="icon-container">
+                <img src="../assets/imgs/log-in.svg" alt="">
+                <p>Log in</p>
+            </RouterLink>
+            <RouterLink v-if="!loggedinUser" to="/signup" class="icon-container">
+                <img src="../assets/imgs/sign-up.svg" alt="">
+                <p>Signup</p>
+            </RouterLink>
+            <RouterLink v-else :to="`/user/${loggedinUser._id}`" class="icon-container anchor">
+                <img src="../assets/imgs/user.svg" alt="">
+                <p>{{ loggedinUser.username }}</p>
+            </RouterLink>
+            <RouterLink to="/about" class="icon-container">
+                <img src="../assets/imgs/about.svg" alt="">
+                <p>About</p>
+            </RouterLink>
+            <RouterLink v-if="loggedinUser" to="/" @click="logout()" class="icon-container">
+                <img src="../assets/imgs/log-out.svg" alt="">
+                <p>Log out</p>
+            </RouterLink>
         </nav>
         <div v-if="openNav" class="overlay" @click="openNav = !openNav"></div>
     </header>
